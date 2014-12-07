ca57997c1b9c815e0d0548c047f176262404183d
ISevenSinsI <Rrreaper@hotmail.com>
2014-12-07 13:40:15 +0100

    shit voorbereid

--- admin/base/menu.php
+++ admin/base/menu.php
@@ -7,6 +7,42 @@
     </div>
 </div>
 
+<div class="menu-item first">
+    <div class="menu-item-inner">
+        <a class="pure-button secondary-button" href="../locations/overview.php">
+            <i class="fa fa-building-o fa-2x"></i>
+            <p>Locaties</p>
+        </a>
+    </div>
+</div>
+
+<div class="menu-item first">
+    <div class="menu-item-inner">
+        <a class="pure-button secondary-button" href="../products/overview.php">
+            <i class="fa fa-tag fa-2x"></i>
+            <p>Artikelen</p>
+        </a>
+    </div>
+</div>
+
+<div class="menu-item first">
+    <div class="menu-item-inner">
+        <a class="pure-button secondary-button" href="../stock/">
+            <i class="fa fa-inbox fa-2x"></i>
+            <p>Voorraad</p>
+        </a>
+    </div>
+</div>
+
+<div class="menu-item first">
+    <div class="menu-item-inner">
+        <a class="pure-button secondary-button" href="../administration/">
+            <i class="fa fa-euro fa-2x"></i>
+            <p>Administratie</p>
+        </a>
+    </div>
+</div>
+
 <div class="menu-item last">
     <div class="menu-item-inner">
         <a class="pure-button secondary-button" href="../logout.php">
---
+++ admin/products/index.php
@@ -0,0 +1,4 @@
+<?php
+require("../functions/products.php"); 
+header("location: overview.php");
+?>
