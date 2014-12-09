fc29a0829e6e4edd2f6a8aad6c6e92d1ea8f8d9e
dutchlander <ruudvissers@hotmail.com>
2014-12-09 17:39:17 +0100

    nieuwe db

--- toolsforever.sql
+++ toolsforever.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2014 at 09:34 PM
+-- Generation Time: Dec 09, 2014 at 05:38 PM
 -- Server version: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -40,32 +40,77 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 
 INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
 ('a0ba57b123c8d5bbd4bd269eea75eae4', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36', '1417717572', 'a:1:{s:9:"user_data";s:0:"";}'),
-('467a0ed924735b00b84c03abdc13442d', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36', '1417725018', '');
+('467a0ed924735b00b84c03abdc13442d', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36', '1417725018', ''),
+('26de64d35fb8faa83dc0f288927f6711', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36', '1417766788', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menu`
+-- Table structure for table `factories`
 --
 
-CREATE TABLE IF NOT EXISTS `menu` (
+CREATE TABLE IF NOT EXISTS `factories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `link_title` varchar(30) NOT NULL,
-  `display_name` varchar(30) NOT NULL,
-  `active` int(1) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `deleted` datetime NOT NULL,
+  `factory` varchar(32) NOT NULL,
+  `phone` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `menu`
+-- Dumping data for table `factories`
 --
 
-INSERT INTO `menu` (`id`, `link_title`, `display_name`, `active`, `created`, `updated`, `deleted`) VALUES
-(1, 'users', 'Gebruikers', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'products', 'Producten', 1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `factories` (`id`, `factory`, `phone`) VALUES
+(1, 'Bosch', '0411-454647'),
+(2, 'Black & Dekker', '0909-0538'),
+(3, 'Precision', '0800-0909');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
+
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `location` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`id`, `location`) VALUES
+(1, 'Rotterdam'),
+(2, 'Almere'),
+(3, 'Eindhoven');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product` varchar(32) NOT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  `factory_id` int(11) NOT NULL,
+  `buy_price` decimal(11,2) NOT NULL,
+  `sell_price` decimal(11,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `eerste` (`factory_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product`, `type`, `factory_id`, `buy_price`, `sell_price`) VALUES
+(1, 'Boormachine', 'XM-1022', 1, '40.00', '60.00'),
+(2, 'Hamer', 'MX-2011', 2, '15.00', '70.00'),
+(3, 'Schroevendraaier', '2020-MM', 3, '23.00', '55.00');
 
 -- --------------------------------------------------------
 
@@ -87,9 +132,32 @@ CREATE TABLE IF NOT EXISTS `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `created`, `updated`, `deleted`) VALUES
-(1, 'development', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'administrator', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(3, 'user', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+(1, 'Development', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'Directie', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(3, 'Gebruiker', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE IF NOT EXISTS `stock` (
+  `location_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `amount` int(11) DEFAULT NULL,
+  KEY `tweede` (`location_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`location_id`, `product_id`, `amount`) VALUES
+(1, 2, 50),
+(1, 2, 45),
+(3, 3, 46),
+(2, 3, 12);
 
 -- --------------------------------------------------------
 
@@ -99,27 +167,40 @@ INSERT INTO `roles` (`id`, `name`, `created`, `updated`, `deleted`) VALUES
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `role_id` int(11) NOT NULL,
-  `phone` varchar(30) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `zipcode` varchar(7) NOT NULL,
-  `city` varchar(30) NOT NULL,
-  `country` varchar(30) NOT NULL,
-  `created` datetime NOT NULL,
-  `updated` datetime NOT NULL,
-  `deleted` datetime NOT NULL,
+  `initials` varchar(32) NOT NULL,
+  `prefix` varchar(32) DEFAULT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `deleted` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role_id`, `phone`, `address`, `zipcode`, `city`, `country`, `created`, `updated`, `deleted`) VALUES
-(1, 'development', 'development@fastdevelopment.nl', '5c9a986a9128ea614568b91a7960c69c608fb01b', 1, '06-5875623', 'hoge heuvel 12', '1234 AB', 'Veghel', 'Nederland', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `users` (`id`, `role_id`, `initials`, `prefix`, `last_name`, `username`, `password`, `deleted`) VALUES
+(1, 1, 'J', 'De', 'Man', 'Jorie', '3c649185ca41b2c53060c8266b0845206b4ab363', 0),
+(2, 1, 'R', '', 'Vissers', 'Ruud', 'b0ea5de4163aab11169c3edc780644cfc79dd7b20', 0),
+(3, 2, 'F', '', 'Verwaard', 'Francy', '2356e59638c3bc00ed8b72d433dbf0b7ecedc536', 0);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`factory_id`) REFERENCES `factories` (`id`);
+
+--
+-- Constraints for table `stock`
+--
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
