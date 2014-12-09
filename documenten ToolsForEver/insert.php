54d039cc3b7ec3252ec587e9b4a9e6f7766f7ba9
ISevenSinsI <Rrreaper@hotmail.com>
2014-12-09 12:42:12 +0100

    insert.php

--- documenten ToolsForEver/insert.php
+++ documenten ToolsForEver/insert.php
@@ -1,5 +1,17 @@
 <?php 
 $sql = "
+	/* Voer gegevens in de tabel factories in */
+
+	insert into factories(`factory`, `phone`)
+			values('Bosch', '0411-454647'),
+				  ('Black & Dekker', '0909-0538'),
+				  ('Precision', '0800-0909');
+
+    /* Voer gegevens in de tabel users in */
+	insert into users(`initials`, `prefix`, `last_name`, `username`, `password`)
+			values('J', 'De', 'Man', 'Jorie', '3c649185ca41b2c53060c8266b0845206b4ab363'),
+				  ('R', '', 'Vissers', 'Ruud', 'b0ea5de4163aab11169c3edc780644cfc79dd7b20'),
+				  ('F', '', 'Verwaard', 'Francy', '2356e59638c3bc00ed8b72d433dbf0b7ecedc536');		
 	
 	/* Voer gegevens in de tabel Locations in */
 
@@ -9,15 +21,6 @@ $sql = "
 				  ('Eindhoven');
 
 
-	/* Voer gegevens in de tabel stock in */
-
-	insert into stock(`location_id`, `product_id`, `amount`)
-			values('1', '1', '25'),
-				  ('1', '2', '45'),
-				  ('3', '3', '46'),
-				  ('2', '3', '12');
-
-
 	/* Voer gegevens in de tabel products in */
 
 	insert into products(`product`, `type`, `factory_id`, `buy_price`, `sell_price`)
@@ -25,20 +28,14 @@ $sql = "
 				  ('Hamer', 'MX-2011', '2', '15', '70'),
 				  ('Schroevendraaier', '2020-MM', '3', '23', '55');
 
+	/* Voer gegevens in de tabel stock in */
 
-    /* Voer gegevens in de tabel factories in */
-
-	insert into factories(`factory`, `phone`)
-			values('Bosch', '0411-454647'),
-				  ('Black & Dekker', '0909-0538'),
-				  ('Precision', '0800-0909');
-
-
-    /* Voer gegevens in de tabel users in */
-	insert into users(`initials`, `prefix`, `last_name`, `username`, `password`)
-			values('J', 'De', 'Man', 'Jorie', '3c649185ca41b2c53060c8266b0845206b4ab363'),
-				  ('R', '', 'Vissers', 'Ruud', 'b0ea5de4163aab11169c3edc780644cfc79dd7b20'),
-				  ('F', '', 'Verwaard', 'Francy', '2356e59638c3bc00ed8b72d433dbf0b7ecedc536');			  
+	insert into stock(`location_id`, `product_id`, `amount`)
+			values('1', '1', '25'),
+				  ('1', '2', '45'),
+				  ('3', '3', '46'),
+				  ('2', '3', '12');
+    	  
 
 ";	
 ?>
